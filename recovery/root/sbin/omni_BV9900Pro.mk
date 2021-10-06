8db916eb94194fc2f3f09cc58cc81d3cef0520e5
Zarin1480 <Robert.l.day@gmail.com>
2021-10-05 20:55:33 -0400

    TWRP for Blackview BV9900Pro
    This may, but likely will not, work.

---
+++ recovery/root/sbin/omni_BV9900Pro.mk
@@ -0,0 +1,34 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace $$DEVICE$$ with your Device Name's Value.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/Blackview/BV9900Pro/prebuilt/zImage:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := BV9900Pro
+PRODUCT_NAME := omni_BV9900Pro
+PRODUCT_BRAND := Blackview
+PRODUCT_MODEL := BV9900Pro
+PRODUCT_MANUFACTURER := Blackview
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="Blackview/BV9900Pro_EEA/BV9900Pro:10/QP1A.190711.020/1604396487:user/release-keys" \
+    PRIVATE_BUILD_DESC="BV9900Pro_EEA_S900AA_V1.0_20201105V07"
